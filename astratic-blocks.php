a26b783f7e4bc18b2d7f0e583df66f7d0c897151
Krzysztof Hęciek <krzysztof.heciek@coditive.com>
2023-10-20 11:57:51 +0200

    1.6.2

--- astratic-blocks.php
+++ astratic-blocks.php
@@ -12,7 +12,7 @@
  * Requires PHP: 7.4
  */
 
-define('ASBL_VERSION', '1.6.1.3');
+define('ASBL_VERSION', '1.6.2');
 define('ASBL_ROOT_PATH', dirname(__FILE__));
 define('ASBL_ASSETS_PATH', dirname(__FILE__) . '/dist');
 define('ASBL_RESOURCES_PATH', dirname(__FILE__) . '/resources');
